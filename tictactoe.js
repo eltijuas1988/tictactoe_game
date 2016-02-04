3f0b7d34a422346ce3b1d247dc0b9407197f846c
LEARN <learn@LEARN06.local>
2016-02-04 14:09:33 -0800

    added functions to test for winning combinations

--- tictactoe.js
+++ tictactoe.js
@@ -1,9 +1,55 @@
 $(document).ready(function(){
-
+  /* variable for how many turns are left in the game */
   var counter = 9;
 
+  /* Function that checks for a win, lose or stalemate */
+  var board = [
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+  ];
 
+
+/* Function determines if there is a winning combination on the board */
+
+  function winLoseOrStalemate() {
+    winCombo1 = board[0] + board[1] + board[2];
+    winCombo2 = board[3] + board[4] + board[5];
+    winCombo3 = board[6] + board[7] + board[8];
+    winCombo4 = board[0] + board[3] + board[6];
+    winCombo5 = board[1] + board[4] + board[7];
+    winCombo6 = board[2] + board[5] + board[8];
+    winCombo7 = board[0] + board[4] + board[8];
+    winCombo8 = board[2] + board[4] + board[6];
+
+    var allWinningCombinations = [
+      winCombo1 = board[0] + board[1] + board[2],
+      winCombo2 = board[3] + board[4] + board[5],
+      winCombo3 = board[6] + board[7] + board[8],
+      winCombo4 = board[0] + board[3] + board[6],
+      winCombo5 = board[1] + board[4] + board[7],
+      winCombo6 = board[2] + board[5] + board[8],
+      winCombo7 = board[0] + board[4] + board[8],
+      winCombo8 = board[2] + board[4] + board[6],
+    ];
+
+    for(var i = 0; i < allWinningCombinations.length; i++ ){
+      console.log(allWinningCombinations[i]);
+    }
+
+    if(winCombo1 === 3){
+      alert("player X wins " + winCombo1);
+    } else if(winCombo1 === -3) {
+      alert("player O wins " + winCombo1);
+    }
+  };
+
+
+
+
+  /* Function that tracks how many turns are left. */
   function gameOverCounter () {
+    /* If none, tells the user that the game is over */
     if(counter === 0){
       $("#currentPlayer").html("GAME OVER");
       $("#messages").html("Thanks for playing");
@@ -14,107 +60,113 @@ $(document).ready(function(){
   }
 
 
-  function addMark(field) {
+  function addMark(field, index) {
 
     if(counter % 2 === 1) {
       $("#currentPlayer").html("Player 1's Turn ");
-      $(field).html('<img src="/Users/crimsondex/Desktop/Challenges/tictactoegame/letterX.png" />');
+      $(field).html('<img src="letterX.png" />');
+      board[index] = 1
       counter--;
     }
 
     else if(counter % 2 === 0) {
       $("#currentPlayer").html("Player 2's Turn ");
-      $(field).html('<img src="/Users/crimsondex/Desktop/Challenges/tictactoegame/letterO.png" />');
+      $(field).html('<img src="letterO.png" />');
+      board[index] = -1
       counter--;
     }
   };
-
-
+//********************* ALL BUTTONS FOR GRID *************//
+// Array index 0
   $("#field1Button").click(function(){
     $("#field1Button").unbind();
+
     //May not need line below
     var usable = {clicked:true};
     // $("#field2").addClass("X");
-    addMark("#field1Button");
+    addMark("#field1Button", 0);
+    winLoseOrStalemate();
     gameOverCounter();
+
   });
 
-
+  // Array index 1
   $("#field2Button").click(function(){
     $("#field2Button").unbind();
     var usable = {clicked:true};
     // $("#field2").addClass("X");
-    addMark("#field2Button");
+    addMark("#field2Button", 1);
+    winLoseOrStalemate();
     gameOverCounter();
   });
 
-
+  // Array index 2
   $("#field3Button").click(function(){
     $("#field3Button").unbind();
     var usable = {clicked:true};
     // $("#field2").addClass("X");
-    addMark("#field3Button");
+    addMark("#field3Button", 2);
+    winLoseOrStalemate();
     gameOverCounter();
   });
 
-
+  // Array index 3
   $("#field4Button").click(function(){
     $("#field4Button").unbind();
     var usable = {clicked:true};
     // $("#field2").addClass("X");
-    addMark("#field4Button");
+    addMark("#field4Button", 3);
+    winLoseOrStalemate();
     gameOverCounter();
   });
 
-
+  // Array index 4
   $("#field5Button").click(function(){
     $("#field5Button").unbind();
     var usable = {clicked:true};
     // $("#field2").addClass("X");
-    addMark("#field5Button");
+    addMark("#field5Button", 4);
+    winLoseOrStalemate();
     gameOverCounter();
   });
 
-
+  // Array index 5
   $("#field6Button").click(function(){
     $("#field6Button").unbind();
     var usable = {clicked:true};
     // $("#field2").addClass("X");
-    addMark("#field6Button");
+    addMark("#field6Button", 5);
+    winLoseOrStalemate();
     gameOverCounter();
   });
 
-
+  // Array index 6
   $("#field7Button").click(function(){
     $("#field7Button").unbind();
     var usable = {clicked:true};
     // $("#field2").addClass("X");
-    addMark("#field7Button");
+    addMark("#field7Button", 6);
+    winLoseOrStalemate();
     gameOverCounter();
   });
 
-
+  // Array index 7
   $("#field8Button").click(function(){
     $("#field8Button").unbind();
     var usable = {clicked:true};
     // $("#field2").addClass("X");
-    addMark("#field8Button");
+    addMark("#field8Button", 7);
+    winLoseOrStalemate();
     gameOverCounter();
   });
 
-
+  // Array index 8
   $("#field9Button").click(function(){
     $("#field9Button").unbind();
     var usable = {clicked:true};
     // $("#field2").addClass("X");
-    addMark("#field9Button");
+    addMark("#field9Button", 8);
+    winLoseOrStalemate();
     gameOverCounter();
   });
-
-  /* if don't want to go square by square then need to experiment with wrapping grid in div and applying logic of disabling boxes w/ X or O in it to whole grid */
-
-  /* if counter % 2 = 0, then it's an X
-  if counter % 2 = 1, then it's an O */
-
-  //unbind fuction will remove the funtion from the click
 });
